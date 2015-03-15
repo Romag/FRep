21862f2cd8d470fc3efa5092c3b2b07906d28cca
Romag <rmagerramov11@gmail.com>
2015-03-15 15:58:57 +0200

    Blah
    Blah

---
+++ Draft/src/Executive.java
@@ -0,0 +1,10 @@
+
+public class Executive {
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			for (int j = 0; j < args.length; j++) {
+				System.out.println(i+j);
+			}
+		}
+	}
+}
