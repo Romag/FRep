86269f941fdd322ce3e08a459935dd3504112d8b
Ramil Magerramov <rmagerramov11@gmail.com>
2016-01-03 10:21:56 +0000

    Arbitrary change to test.

--- Draft/src/Executive.java
+++ Draft/src/Executive.java
@@ -1,9 +1,11 @@
 
 public class Executive {
 	public static void main(String[] args) {
+    System.out.println("Written using Google Cloud Platform.");
 		for (int i = 0; i < args.length; i++) {
 			for (int j = 0; j < args.length; j++) {
 				System.out.println(i+j);
+        
 			}
 		}
 	}
